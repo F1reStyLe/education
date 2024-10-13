5a9e2df2745066b3dc3dc63383b3b583a0bcc475
F1reStyLe <iwgw@bk.ru>
2024-10-13 15:19:38 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  
+  const config = new DocumentBuilder()
+    .setTitle('Flowers API')
+    .setDescription('The flowers API description')
+    .setVersion('1.0')
+    .addTag('flowers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(4200);
-
+  
   const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
